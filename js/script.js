933050fd3935d92e26c532c41683d9f8d239282b
devinhuizingh <devinhuizingh@gmail.com>
2015-12-22 10:20:34 -0700

    working well except need item in cart already

--- js/script.js
+++ js/script.js
@@ -4,20 +4,28 @@ $(document).ready(function() {
 $('button').click(function(e) {
 	e.preventDefault();
 	$('#list').prepend('<li><button class="checkbutton">Got It</button><button class="deletebutton">Delete</button>' + $('input#item').val() +  '</li>');
+	$('#item').val('');
 })
 
 
 
-
-//$('.deletebutton').on('click', 'li', function(event){
-;//})
-var i = 0;
-
-$('#list').on('click', '.deletebutton', i, function(event){
-	console.log ("button clicked");
-	console.log(event);
-	$(this).closest('li').remove();
+$('#list').on('click', '.deletebutton', function(event){
+		$(this).closest('li').remove();
 })
+
+$('#list').on('click', '.checkbutton', function(event){
+	$(this).closest('li').insertBefore($('#test'));
+	$(this).remove();
+
+	
+
+})
+
+$('#gotit').on('click', '.deletebutton', function(event){
+		$(this).closest('li').remove();
+})
+
+$('#gotit >checkbutton').remove()
 
 })
 
