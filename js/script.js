3fc6b36a157ae716f58bafd103fa155fea0b5b2a
devinhuizingh <devinhuizingh@gmail.com>
2015-12-22 10:27:07 -0700

    added semicolons

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ $('button').click(function(e) {
 	e.preventDefault();
 	$('#list').prepend('<li><button class="checkbutton">Got It</button><button class="deletebutton">Delete</button>' + $('input#item').val() +  '</li>');
 	$('#item').val('');
-})
+});
 
 
 
 $('#list').on('click', '.deletebutton', function(event){
 		$(this).closest('li').remove();
-})
+});
 
 $('#list').on('click', '.checkbutton', function(event){
 	$(this).closest('li').insertBefore($('#test'));
@@ -19,15 +19,15 @@ $('#list').on('click', '.checkbutton', function(event){
 
 	
 
-})
+});
 
 $('#gotit').on('click', '.deletebutton', function(event){
 		$(this).closest('li').remove();
-})
+});
 
 $('#gotit >checkbutton').remove()
 
-})
+});
 
 
 
