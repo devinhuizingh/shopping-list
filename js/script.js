9f51cad97dba5b9ae95337525a6d0be061dddd39
devinhuizingh <devinhuizingh@gmail.com>
2016-01-21 14:28:02 -0700

    made it a little prettier and activated drag

--- js/script.js
+++ js/script.js
@@ -1,34 +1,59 @@
 $(document).ready(function() {
 
 
-$('button').click(function(e) {
-	e.preventDefault();
-	$('#list').prepend('<li><button class="checkbutton">Got It</button><button class="deletebutton">Delete</button>' + $('input#item').val() +  '</li>');
-	$('#item').val('');
+	$('button').click(function(e) {
+		e.preventDefault();
+		$('#list').prepend('<li><button class="checkbutton">Got It</button><button class="deletebutton">Delete</button>' + $('input#item').val() +  '</li>');
+		$('#item').val('');
+	});
+
+
+
+	$('#list').on('click', '.deletebutton', function(event){
+			$(this).closest('li').remove();
+	});
+
+	$('#list').on('click', '.checkbutton', function(event){
+		$(this).closest('li').appendTo($('#gotit'));
+		$(this).remove();
+
+		
+
+	});
+
+	$('#gotit').on('click', '.deletebutton', function(event){
+			$(this).closest('li').remove();
+	});
+
+	//$('#gotit >checkbutton').remove()
+
+
+
+ 
+$(function() {
+    $( "#list" ).sortable({
+      revert: true
+    });
+    
+    $( "#list" ).draggable({
+      connectToSortable: "#list",
+      helper: "clone",
+      revert: "invalid"
+    });
+    $( "#gotit" ).sortable({
+      revert: true
+    });
+    
+    $( "#gotit" ).draggable({
+      connectToSortable: "#list",
+      helper: "clone",
+      revert: "invalid"
+    });
+    
 });
 
 
-
-$('#list').on('click', '.deletebutton', function(event){
-		$(this).closest('li').remove();
 });
-
-$('#list').on('click', '.checkbutton', function(event){
-	$(this).closest('li').insertBefore($('#test'));
-	$(this).remove();
-
-	
-
-});
-
-$('#gotit').on('click', '.deletebutton', function(event){
-		$(this).closest('li').remove();
-});
-
-$('#gotit >checkbutton').remove()
-
-});
-
 
 
 
